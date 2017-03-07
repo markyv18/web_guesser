dccafcf7700cce5fc05c333f5df654dc559d5d97
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-03-07 12:44:02 -0700

    struggling to get cheat and guess counter to work

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,17 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 NUMBER = rand(100)
+@@count = 5
 
 get '/' do
+  @@count -= 1
+  if @@count == 0
+    guess = nil
+  end
   guess = params["guess"].to_i
-  message = check_guess(guess)
+  cheat = params["cheat"]
+  message = check_guess(guess, cheat)
   color = get_color(guess)
   erb :index, :locals => {:message => message, :guess => guess, :color => color}
 end
 
-def check_guess(guess)
+def check_guess(guess, cheat = nil)
   if guess > 101
-  message = "WAY off. Let's keep it under 100 shall we?"
+    message = "WAY off. Let's keep it under 100 shall we?"
+  elsif cheat == true
+    message "The number is #{NUMBER}, but you cheated!"
   elsif guess > (NUMBER + 10)
     message = " Way too high! You are more than 10 digits too high."
   elsif guess < (NUMBER - 10)
@@ -23,7 +31,10 @@ def check_guess(guess)
     message = "Too high! But within 10 digits!"
   elsif guess == NUMBER
     message = "You got it right! The number is #{NUMBER}."
+  elsif guess == nil
+    message = "Oops! You've used up all your guesses."
   end
+
 end
 
 def get_color(guess)
@@ -35,5 +46,7 @@ def get_color(guess)
     color = "yellow"
   elsif guess == NUMBER
     color = "green"
+  elsif guess == nil
+    color = "white"
   end
 end
